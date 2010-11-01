2a8ad34fb5fb90ed9b7500e34f685dcb4ea7161e
Devaev Maxim <mdevaev@gmail.com>
2010-11-01 17:22:56 +0300

    Новый TabbedTranslateBrowser

--- apps/xsl/src/xsl/TabbedTranslateBrowser.py
+++ apps/xsl/src/xsl/TabbedTranslateBrowser.py
@@ -33,55 +33,55 @@ class TabbedTranslateBrowser(Qt.QWidget) :
 
 		#####
 
-		self._main_layout = Qt.QVBoxLayout()
-		self._main_layout.setContentsMargins(0, 0, 0, 0)
-		self._main_layout.setSpacing(0)
-		self.setLayout(self._main_layout)
+		self.__main_layout = Qt.QVBoxLayout()
+		self.__main_layout.setContentsMargins(0, 0, 0, 0)
+		self.__main_layout.setSpacing(0)
+		self.setLayout(self.__main_layout)
 
 		#####
 
-		self._shred_lock_flag = False
+		self.__shred_lock_flag = False
 
-		self._translate_browsers_list = []
-		self._old_index = -1
+		self.__translate_browsers_list = []
+		self.__old_index = -1
 
-		self._tmp_text_cursor = Qt.QTextCursor()
-
-		#####
-
-		self._tab_widget = Qt.QTabWidget(self)
-		self._tab_widget.setTabsClosable(True)
-		self._main_layout.addWidget(self._tab_widget)
-
-		self._add_tab_button = Qt.QToolButton(self)
-		self._add_tab_button.setIcon(IconsLoader.icon("tab-new"))
-		self._add_tab_button.setIconSize(Qt.QSize(16, 16))
-		self._add_tab_button.setCursor(Qt.Qt.ArrowCursor)
-		self._add_tab_button.setAutoRaise(True)
-		self._tab_widget.setCornerWidget(self._add_tab_button, Qt.Qt.TopLeftCorner)
-
-		self._remove_tab_button = Qt.QToolButton(self)
-		self._remove_tab_button.setIcon(IconsLoader.icon("tab-close"))
-		self._remove_tab_button.setIconSize(Qt.QSize(16, 16))
-		self._remove_tab_button.setCursor(Qt.Qt.ArrowCursor)
-		self._remove_tab_button.setAutoRaise(True)
-		self._tab_widget.setCornerWidget(self._remove_tab_button, Qt.Qt.TopRightCorner)
-
-		self._text_search_frame = TextSearchFrame.TextSearchFrame(self)
-		self._text_search_frame.hide()
-		self._main_layout.addWidget(self._text_search_frame)
+		self.__tmp_text_cursor = Qt.QTextCursor()
 
 		#####
 
-		self.connect(self._add_tab_button, Qt.SIGNAL("clicked()"), self.addTab)
-		self.connect(self._remove_tab_button, Qt.SIGNAL("clicked()"), self.removeTab)
+		self.__tab_widget = Qt.QTabWidget(self)
+		self.__tab_widget.setTabsClosable(True)
+		self.__main_layout.addWidget(self.__tab_widget)
 
-		self.connect(self._tab_widget, Qt.SIGNAL("currentChanged(int)"), self.tabChanged)
-		self.connect(self._tab_widget, Qt.SIGNAL("tabCloseRequested(int)"), self.removeTab)
+		self.__add_tab_button = Qt.QToolButton(self)
+		self.__add_tab_button.setIcon(IconsLoader.icon("tab-new"))
+		self.__add_tab_button.setIconSize(Qt.QSize(16, 16))
+		self.__add_tab_button.setCursor(Qt.Qt.ArrowCursor)
+		self.__add_tab_button.setAutoRaise(True)
+		self.__tab_widget.setCornerWidget(self.__add_tab_button, Qt.Qt.TopLeftCorner)
 
-		self.connect(self._text_search_frame, Qt.SIGNAL("findNextRequest(const QString &)"), self.findNext)
-		self.connect(self._text_search_frame, Qt.SIGNAL("findPreviousRequest(const QString &)"), self.findPrevious)
-		self.connect(self._text_search_frame, Qt.SIGNAL("instantSearchRequest(const QString &)"), self.instantSearch)
+		self.__remove_tab_button = Qt.QToolButton(self)
+		self.__remove_tab_button.setIcon(IconsLoader.icon("tab-close"))
+		self.__remove_tab_button.setIconSize(Qt.QSize(16, 16))
+		self.__remove_tab_button.setCursor(Qt.Qt.ArrowCursor)
+		self.__remove_tab_button.setAutoRaise(True)
+		self.__tab_widget.setCornerWidget(self.__remove_tab_button, Qt.Qt.TopRightCorner)
+
+		self.__text_search_frame = TextSearchFrame.TextSearchFrame(self)
+		self.__text_search_frame.hide()
+		self.__main_layout.addWidget(self.__text_search_frame)
+
+		#####
+
+		self.connect(self.__add_tab_button, Qt.SIGNAL("clicked()"), self.addTab)
+		self.connect(self.__remove_tab_button, Qt.SIGNAL("clicked()"), self.removeTab)
+
+		self.connect(self.__tab_widget, Qt.SIGNAL("currentChanged(int)"), self.tabChanged)
+		self.connect(self.__tab_widget, Qt.SIGNAL("tabCloseRequested(int)"), self.removeTab)
+
+		self.connect(self.__text_search_frame, Qt.SIGNAL("findNextRequest(const QString &)"), self.findNext)
+		self.connect(self.__text_search_frame, Qt.SIGNAL("findPreviousRequest(const QString &)"), self.findPrevious)
+		self.connect(self.__text_search_frame, Qt.SIGNAL("instantSearchRequest(const QString &)"), self.instantSearch)
 
 		#####
 
@@ -91,148 +91,146 @@ class TabbedTranslateBrowser(Qt.QWidget) :
 	### Public ###
 
 	def setShredLock(self, shred_lock_flag) :
-		self._shred_lock_flag = shred_lock_flag
+		self.__shred_lock_flag = shred_lock_flag
 
 	def showTextSearchFrame(self) :
-		self._text_search_frame.show()
+		self.__text_search_frame.show()
 
 	def hideTextSearchFrame(self) :
-		self._text_search_frame.hide()
+		self.__text_search_frame.hide()
 
 	###
 
 	def addTab(self) :
-		self._translate_browsers_list.append(TranslateBrowser.TranslateBrowser(self))
-		index = len(self._translate_browsers_list) - 1
+		translate_browser = TranslateBrowser.TranslateBrowser(self.__tab_widget)
+		self.__translate_browsers_list.append(translate_browser)
+		index = len(self.__translate_browsers_list) - 1
 
-		self.connect(self._translate_browsers_list[index], Qt.SIGNAL("newTabRequest()"), self.addTab)
-		self.connect(self._translate_browsers_list[index], Qt.SIGNAL("uFindRequest(const QString &)"), self.uFindRequestSignal)
-		self.connect(self._translate_browsers_list[index], Qt.SIGNAL("cFindRequest(const QString &)"), self.cFindRequestSignal)
-		self.connect(self._translate_browsers_list[index], Qt.SIGNAL("statusChanged(const QString &)"), self.statusChangedSignal)
-		self.connect(self._translate_browsers_list[index], Qt.SIGNAL("showTextSearchFrameRequest()"), self._text_search_frame.show)
-		self.connect(self._translate_browsers_list[index], Qt.SIGNAL("hideTextSearchFrameRequest()"), self._text_search_frame.hide)
-		self.connect(self._translate_browsers_list[index], Qt.SIGNAL("setFoundRequest(bool)"), self._text_search_frame.setFound)
+		self.connect(translate_browser, Qt.SIGNAL("newTabRequest()"), self.addTab)
+		self.connect(translate_browser, Qt.SIGNAL("uFindRequest(const QString &)"), self.uFindRequestSignal)
+		self.connect(translate_browser, Qt.SIGNAL("cFindRequest(const QString &)"), self.cFindRequestSignal)
+		self.connect(translate_browser, Qt.SIGNAL("statusChanged(const QString &)"), self.statusChangedSignal)
+		self.connect(translate_browser, Qt.SIGNAL("showTextSearchFrameRequest()"), self.__text_search_frame.show)
+		self.connect(translate_browser, Qt.SIGNAL("hideTextSearchFrameRequest()"), self.__text_search_frame.hide)
+		self.connect(translate_browser, Qt.SIGNAL("setFoundRequest(bool)"), self.__text_search_frame.setFound)
 
-		self._translate_browsers_list[index].setText(tr("<font class=\"info_font\">Empty</font>"))
-		self._tab_widget.addTab(self._translate_browsers_list[index], tr("(Untitled)"))
-		self._tab_widget.setCurrentIndex(index)
+		translate_browser.setText(tr("<font class=\"info_font\">Empty</font>"))
+		self.__tab_widget.addTab(translate_browser, tr("(Untitled)"))
+		self.__tab_widget.setCurrentIndex(index)
 
 	def removeTab(self, index = -1) :
-		if self._shred_lock_flag :
+		if self.__shred_lock_flag :
 			return
 
-		index = ( self._tab_widget.currentIndex() if index < 0 else index )
+		index = ( self.__tab_widget.currentIndex() if index < 0 else index )
 
-		self._tab_widget.removeTab(index)
-		translate_browser = self._translate_browsers_list.pop(index)
+		self.__tab_widget.removeTab(index)
+		translate_browser = self.__translate_browsers_list.pop(index)
 		del translate_browser
 
-		if self._tab_widget.count() == 0 :
+		if self.__tab_widget.count() == 0 :
 			self.addTab()
 
 	###
 
 	def count(self) :
-		return self._tab_widget.count()
+		return self.__tab_widget.count()
 
 	def currentIndex(self) :
-		return self._tab_widget.currentIndex()
+		return self.__tab_widget.currentIndex()
 
 	###
 
 	def setText(self, index, text) :
-		self._translate_browsers_list[index].setText(text)
+		self.__translate_browsers_list[index].setText(text)
 
 	def setCaption(self, index, word) :
-		self._tab_widget.setTabText(index, word)
+		self.__tab_widget.setTabText(index, word)
 
 	###
 
 	def text(self, index = -1) :
-		index = ( self._tab_widget.currentIndex() if index < 0 else index )
-		return self._translate_browsers_list[index].text()
+		index = ( self.__tab_widget.currentIndex() if index < 0 else index )
+		return self.__translate_browsers_list[index].text()
 
 	def caption(self, index = -1) :
-		index = ( self._tab_widget.currentIndex() if index < 0 else index )
-		return self._tab_widget.tabText(index)
+		index = ( self.__tab_widget.currentIndex() if index < 0 else index )
+		return self.__tab_widget.tabText(index)
 
 	def browser(self, index = -1) :
-		index = ( self._tab_widget.currentIndex() if index < 0 else index )
-		return self._translate_browsers_list[index]
+		index = ( self.__tab_widget.currentIndex() if index < 0 else index )
+		return self.__translate_browsers_list[index]
 
 	def document(self, index = -1) :
-		index = ( self._tab_widget.currentIndex() if index < 0 else index )
-		return self._translate_browsers_list[index].document()
+		index = ( self.__tab_widget.currentIndex() if index < 0 else index )
+		return self.__translate_browsers_list[index].document()
 
 	###
 
 	def clearPage(self, index = -1) :
-		if self._shred_lock_flag :
+		if self.__shred_lock_flag :
 			return
 
-		index = ( self._tab_widget.currentIndex() if index < 0 else index )
-		self._translate_browsers_list[index].setText(tr("<font class=\"info_font\">Empty</font>"))
-		self._tab_widget.setTabText(index, tr("(Untitled)"))
+		index = ( self.__tab_widget.currentIndex() if index < 0 else index )
+		self.__translate_browsers_list[index].setText(tr("<font class=\"info_font\">Empty</font>"))
+		self.__tab_widget.setTabText(index, tr("(Untitled)"))
 
 	def clearAll(self) :
-		if self._shred_lock_flag :
+		if self.__shred_lock_flag :
 			return
 
 		for count in xrange(self.count()) :
 			self.removeTab(0)
 
-		self._text_search_frame.hide()
-		self._text_search_frame.clear()
+		self.__text_search_frame.hide()
+		self.__text_search_frame.clear()
 
 	def clear(self, index = -1) :
-		if self._shred_lock_flag :
+		if self.__shred_lock_flag :
 			return
 
-		index = ( self._tab_widget.currentIndex() if index < 0 else index )
-		self._translate_browsers_list[index].clear()
-		self._tab_widget.setTabText(index, Qt.QString())
+		index = ( self.__tab_widget.currentIndex() if index < 0 else index )
+		self.__translate_browsers_list[index].clear()
+		self.__tab_widget.setTabText(index, Qt.QString())
 
 	def clearSpecials(self, index = -1) :
-		if self._shred_lock_flag :
+		if self.__shred_lock_flag :
 			return
 
-		index = ( self._tab_widget.currentIndex() if index < 0 else index )
-		self._translate_browsers_list[index].clearSpecials()
+		index = ( self.__tab_widget.currentIndex() if index < 0 else index )
+		self.__translate_browsers_list[index].clearSpecials()
 
 	###
 
 	def zoomIn(self, index = -1, range = 1) :
-		index = ( self._tab_widget.currentIndex() if index < 0 else index )
-		self._translate_browsers_list[index].zoomIn(range)
+		index = ( self.__tab_widget.currentIndex() if index < 0 else index )
+		self.__translate_browsers_list[index].zoomIn(range)
 
 	def zoomOut(self, index = -1, range = 1) :
-		index = ( self._tab_widget.currentIndex() if index < 0 else index )
-		self._translate_browsers_list[index].zoomOut(range)
+		index = ( self.__tab_widget.currentIndex() if index < 0 else index )
+		self.__translate_browsers_list[index].zoomOut(range)
 
 	def zoomNormal(self, index = -1) :
-		index = ( self._tab_widget.currentIndex() if index < 0 else index )
-		self._translate_browsers_list[index].zoomNormal()
+		index = ( self.__tab_widget.currentIndex() if index < 0 else index )
+		self.__translate_browsers_list[index].zoomNormal()
 
 
 	### Private ###
 
 	def findNext(self, word) :
-		index = self.currentIndex()
-		self._translate_browsers_list[index].findNext(word)
+		self.__translate_browsers_list[self.currentIndex()].findNext(word)
 
 	def findPrevious(self, word) :
-		index = self.currentIndex()
-		self._translate_browsers_list[index].findPrevious(word)
+		self.__translate_browsers_list[self.currentIndex()].findPrevious(word)
 
 	def instantSearch(self, word) :
-		index = self.currentIndex()
-		self._translate_browsers_list[index].instantSearch(word)
+		self.__translate_browsers_list[self.currentIndex()].instantSearch(word)
 
 	###
 
 	def tabChanged(self, index) :
-		self._translate_browsers_list[self._old_index].clearSpecials()
-		self._old_index = index
+		self.__translate_browsers_list[self.__old_index].clearSpecials()
+		self.__old_index = index
 		self.tabChangedSignal(index)
 
 

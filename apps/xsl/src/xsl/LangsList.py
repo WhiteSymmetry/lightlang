b19e83b50c7b0776b4fb127770b3ca4c95c8d7ff
Devaev Maxim <mdevaev@gmail.com>
2010-11-02 16:35:54 +0300

    LangsList без сохранения состояний

--- apps/xsl/src/xsl/LangsList.py
+++ apps/xsl/src/xsl/LangsList.py
@@ -23,103 +23,60 @@
 import Qt
 
 
-##### Private classes #####
-class LangsListMultiple(Qt.QObject) :
-	def __init__(self, parent = None) :
-		Qt.QObject.__init__(self, parent)
-
-		#####
-
-		self.__langs_list = []
-		self.__lang_codes_dict = {}
-
-		#####
-
-		self.translateObject()
-
-
-	### Public ###
-
-	def langs(self) :
-		return self.__langs_list
-
-	def langName(self, short_name) :
-		short_name = str(short_name)
-
-		if self.__lang_codes_dict.has_key(short_name) :
-			return Qt.QString(self.__lang_codes_dict[short_name])
-		else :
-			return Qt.QString(short_name)
-
-
-	### Private ###
-
-	def translateObject(self) :
-		self.__lang_codes_dict = {
-			"af" : tr("Afrikaans"), "sq" : tr("Albanian"), "ar" : tr("Arabic"), "be" : tr("Belarusian"),
-			"bg" : tr("Bulgarian"), "ca" : tr("Catalan"), "zh" : tr("Chinese"), "hz" : tr("Croatian"),
-			"cs" : tr("Czech"), "da" : tr("Danish"), "nl" : tr("Dutch"), "en" : tr("English"),
-			"et" : tr("Estonian"), "tl" : tr("Filipino"), "fi" : tr("Finnish"), "fr" : tr("French"),
-			"gl" : tr("Galician"), "de" : tr("German"), "el" : tr("Greek"), "iw" : tr("Hebrew"),
-			"hi" : tr("Hindi"), "hu" : tr("Hungarian"), "is" : tr("Icelandic"), "id" : tr("Indonesian"),
-			"ga" : tr("Irish"), "it" : tr("Italian"), "ja" : tr("Japanese"), "ko" : tr("Korean"),
-			"lv" : tr("Latvian"), "lt" : tr("Lithuanian"), "mk" : tr("Macedonian"), "ms" : tr("Malay"),
-			"mt" : tr("Maltese"), "no" : tr("Norwegian"), "fa" : tr("Persian"), "pl" : tr("Polish"),
-			"pt" : tr("Portuguese"), "ro" : tr("Romanian"), "ru" : tr("Russian"), "sr" : tr("Serbian"),
-			"sk" : tr("Slovak"), "sl" : tr("Slovenian"), "es" : tr("Spanish"), "sw" : tr("Swahili"),
-			"sv" : tr("Swedish"), "th" : tr("Thai"), "tr" : tr("Turkish"), "uk" : tr("Ukrainian"),
-			"vi" : tr("Vietnamese"), "cy" : tr("Welsh"), "yi" : tr("Yiddish")
-		}
-
-
-		self.__langs_list = []
-		for lang_codes_dict_key in self.__lang_codes_dict.keys() :
-			self.__langs_list.append({
-				"name" : Qt.QString(self.__lang_codes_dict[lang_codes_dict_key]),
+##### Public methods #####
+def langs() :
+	lang_codes_dict = langCodes()
+	langs_list = []
+	for lang_codes_dict_key in lang_codes_dict.keys() :
+		langs_list.append({
+				"name" : Qt.QString(lang_codes_dict[lang_codes_dict_key]),
 				"code" : Qt.QString(lang_codes_dict_key)
-				})
+			})
+	sortLangsList(langs_list)
+	return langs_list
 
-		self.sortLangsList(self.__langs_list)
+def langName(short_name) :
+	short_name = str(short_name)
+	lang_codes_dict = langCodes()
+	if lang_codes_dict.has_key(short_name) :
+		return Qt.QString(lang_codes_dict[short_name])
+	else :
+		Qt.QString(short_name)
 
-	def sortLangsList(self, langs_list, left = None, right = None) :
-		if left == right == None :
-			left = 0
-			right = len(langs_list) - 1
-
-		if left >= right :
-			return
-
-		i = j = left
-		while j <= right :
-			if langs_list[j]["name"] <= langs_list[right]["name"] :
-				(langs_list[i], langs_list[j]) = (langs_list[j], langs_list[i])
-				i += 1
-			j += 1
-
-		self.sortLangsList(langs_list, left, i - 2)
-		self.sortLangsList(langs_list, i, right)
-
-
-	### Handlers ###
-
-	def event(self, event) :
-		if event.type() == Qt.QEvent.LanguageChange :
-			self.translateObject()
-			return True
-		else :
-			return Qt.QObject.event(self, event)
+def langCodes() :
+	return {
+		"af" : tr("Afrikaans"), "sq" : tr("Albanian"), "ar" : tr("Arabic"), "be" : tr("Belarusian"),
+		"bg" : tr("Bulgarian"), "ca" : tr("Catalan"), "zh" : tr("Chinese"), "hz" : tr("Croatian"),
+		"cs" : tr("Czech"), "da" : tr("Danish"), "nl" : tr("Dutch"), "en" : tr("English"),
+		"et" : tr("Estonian"), "tl" : tr("Filipino"), "fi" : tr("Finnish"), "fr" : tr("French"),
+		"gl" : tr("Galician"), "de" : tr("German"), "el" : tr("Greek"), "iw" : tr("Hebrew"),
+		"hi" : tr("Hindi"), "hu" : tr("Hungarian"), "is" : tr("Icelandic"), "id" : tr("Indonesian"),
+		"ga" : tr("Irish"), "it" : tr("Italian"), "ja" : tr("Japanese"), "ko" : tr("Korean"),
+		"lv" : tr("Latvian"), "lt" : tr("Lithuanian"), "mk" : tr("Macedonian"), "ms" : tr("Malay"),
+		"mt" : tr("Maltese"), "no" : tr("Norwegian"), "fa" : tr("Persian"), "pl" : tr("Polish"),
+		"pt" : tr("Portuguese"), "ro" : tr("Romanian"), "ru" : tr("Russian"), "sr" : tr("Serbian"),
+		"sk" : tr("Slovak"), "sl" : tr("Slovenian"), "es" : tr("Spanish"), "sw" : tr("Swahili"),
+		"sv" : tr("Swedish"), "th" : tr("Thai"), "tr" : tr("Turkish"), "uk" : tr("Ukrainian"),
+		"vi" : tr("Vietnamese"), "cy" : tr("Welsh"), "yi" : tr("Yiddish")
+	}
 
 
-##### Public classes #####
-class LangsList(LangsListMultiple) :
-	__langs_list_multiple_object = None
+##### Private methods #####
+def sortLangsList(langs_list, left = None, right = None) :
+	if left == right == None :
+		left = 0
+		right = len(langs_list) - 1
 
-	def __new__(self, parent = None) :
-		if self.__langs_list_multiple_object == None :
-			self.__langs_list_multiple_object = LangsListMultiple.__new__(self, parent)
-			LangsListMultiple.__init__(self.__langs_list_multiple_object, parent)
-		return self.__langs_list_multiple_object
+	if left >= right :
+		return
 
-	def __init__(self, parent = None) :
-		pass
+	i = j = left
+	while j <= right :
+		if langs_list[j]["name"] <= langs_list[right]["name"] :
+			(langs_list[i], langs_list[j]) = (langs_list[j], langs_list[i])
+			i += 1
+		j += 1
+
+	sortLangsList(langs_list, left, i - 2)
+	sortLangsList(langs_list, i, right)
 
